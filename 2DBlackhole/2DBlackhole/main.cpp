6a9b6063b24949c0c23239a160c5f52a6743e1f3
Memecon <jon.byrne27@btinternet.com>
2025-09-20 16:58:26 +0100

    Code from the tutorial that works

--- 2DBlackhole/2DBlackhole/main.cpp
+++ 2DBlackhole/2DBlackhole/main.cpp
@@ -1,4 +1,4 @@
-
+﻿
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -20,7 +20,7 @@ using Clock = std::chrono::high_resolution_clock;
 const double G = 6.67430E-11; //gravitational constant
 const double c = 299792458; //speed of light in m/s
 struct Ray;
-void rk4(Ray& ray, double timeStep, double rs);
+void rk4Step(Ray& ray, double dλ, double rs);
 struct Engine { //sets up opengl engine
 	GLFWwindow* window;
 	int width = 800;
@@ -67,183 +67,162 @@ struct Engine { //sets up opengl engine
 	}
 };
 struct BlackHole {
-	vec2 position;
-	double mass;
-	double r_s; //r_s stands for event horizon
-	//this is the point (distance) where in which the gravitational pull of the black hole is so strong not even light can escape
-	BlackHole(vec2 pos, double m) : position(pos), mass(m) {
-		//event horizon is calculated using the formula r_s = 2GM/c^2
-		//G = gravitational constant
-		//c = speed of light
-		//m = mass of black hole
-		//r_s will act of the radius of the black hole, meaning radius = event horizon
-		r_s = (2.0 * G * m) / (c * c);
-	}
-	void drawBlackHole() { //OpenGL does not render circles and so drawing of circles must be calculated manually
-		glBegin(GL_TRIANGLE_FAN); //specifies the primitives/group of primitives
-		glColor3f(1.0f, 0.0f, 0.0f); //set colour of drawing to red using RGB.
-		
-		glVertex2f(position.x, position.y); //centre vertex of triangle fan
-		for (int i = 0; i <= 100; i++) {
-			float angle = 2.0f * M_PI * i / 100;
-			float x = (r_s * cos(angle)) + position.x; //calculate x position of circle at specific angle
-			float y = (r_s * sin(angle)) + position.y; //calculate y position of circle at specific angle
-			//position values used for offset of circle
-			glVertex2f(x, y); //set position of vertex as position on circle at angle
-		}
-		//GL_TRIANGLE_FAN has a fixed first vertex, with other vertcies drawing a triangle from this vertex
-		glEnd(); //end rendering
-	}
-	double getEventHorizon() { return r_s; }
-};
-BlackHole hole(vec2(0.0f, 0.0f), 8.54e36); //this is meant to represent a black hole called sagittarius A
-//Ray relates to light rays, which are represented by ray tracing
+    vec3 position;
+    double mass;
+    double radius;
+    double r_s;
 
+    BlackHole(vec3 pos, float m) : position(pos), mass(m) { r_s = 2.0 * G * mass / (c * c); }
+    void draw() {
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3f(1.0f, 0.0f, 0.0f);               // Red color for the black hole
+        glVertex2f(0.0f, 0.0f);                    // Center
+        for (int i = 0; i <= 100; i++) {
+            float angle = 2.0f * M_PI * i / 100;
+            float x = r_s * cos(angle); // Radius of 0.1
+            float y = r_s * sin(angle);
+            glVertex2f(x, y);
+        }
+        glEnd();
+    }
+};
+BlackHole hole(vec3(0.0f, 0.0f, 0.0f), 8.54e36); // Sagittarius A black hole
 struct Ray {
-	double x;
-	double y; //cartesian coordinates of the ray
-	double r; //r will be distance from the black hole to the ray
-	double phi; //phi will be the angle between the ray and the black hole
-	double dr;
-	double dphi; //dr and dphi represent the change in r and phi repsectively. This means they are the velcoities in polar coordinates
-	double d2r;
-	double d2phi;
-	double E;
-	double L; //conserved quantities
-	//polar coordinates of the ray
-	vec2 direction;
-	vector<vec2> trails; //this will be used to track prior positions of the ray
-	Ray(vec2 pos, vec2 dir) : x(pos.x), y(pos.y), direction(dir), dr(dir.x), dphi(dir.y) {
-		r = sqrt((x * x) + (y * y)); //this assumes the black hole is at the origin (0,0) which is currently the case in the simulation
-		phi = atan2(y, x); //atan2 will get the angle in radians between x and y component of the ray
-		
-		//dr = c * cos(phi) + direction.y * sin(phi); //initial radial velocity of the ray, calculated using dot product of direction vector and unit vector in radial direction
-		//dphi = (-c * sin(phi) + direction.y * cos(phi)) / r; //initial angular velocity of the ray, calculated using dot product of direction vector and unit vector in angular direction
-		
-		dr = direction.x * cos(phi) + direction.y * sin(phi); //initial radial velcoity of ray
-		dphi = (-direction.x * sin(phi) + direction.y * cos(phi)) / r;
-		L = r * r * dphi;
-		double f = 1.0 - hole.r_s / r;
-		double dt_dl = sqrt((dr * dr) / (f * f) + (r * r * dphi * dphi) / f);
-		E = f * dt_dl;
-		trails.push_back({ x, y });
-	}
-	void drawRay(const vector<Ray>& rays)  {
-		glPointSize(2.0f);
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glBegin(GL_POINTS);
-		for (const auto& ray : rays) {
-			glVertex2f(ray.x, ray.y);
-		}
-		glEnd();
+    // -- cartesian coords -- //
+    double x;   double y;
+    // -- polar coords -- //
+    double r;   double phi;
+    double dr;  double dphi;
+    vector<vec2> trail; // trail of points
+    double E, L;             // conserved quantities
 
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //blend function for transparency
-		glLineWidth(1.0f);
-		for (const auto& ray : rays) {
-			int N = ray.trails.size();
-			if (N < 2) continue; //need at least 2 points to draw a line
-			glBegin(GL_LINE_STRIP); //specifies the primitive is a line
-			for (int i = 0; i < N; ++i) {
-				float alpha = float(i) / float(N - 1); //calculates alpha value
-				glColor4f(1.0f, 1.0f, 1.0f, std::max(alpha, 0.05f));
-				glVertex2f(ray.trails[i].x, ray.trails[i].y); //specifies position of point
+    Ray(vec2 pos, vec2 dir) : x(pos.x), y(pos.y), r(sqrt(pos.x* pos.x + pos.y * pos.y)), phi(atan2(pos.y, pos.x)), dr(dir.x), dphi(dir.y) {
+        // step 1) get polar coords (r, phi) :
+        this->r = sqrt(x * x + y * y);
+        this->phi = atan2(y, x);
+        // step 2) seed velocities :
+        dr = dir.x * cos(phi) + dir.y * sin(phi); // m/s
+        dphi = (-dir.x * sin(phi) + dir.y * cos(phi)) / r;
+        // step 3) store conserved quantities
+        L = r * r * dphi;
+        double f = 1.0 - hole.r_s / r;
+        double dt_dλ = sqrt((dr * dr) / (f * f) + (r * r * dphi * dphi) / f);
+        E = f * dt_dλ;
+        // step 4) start trail :
+        trail.push_back({ x, y });
+    }
+    void draw(const std::vector<Ray>& rays) {
+        // draw current ray positions as points
+        glPointSize(2.0f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glBegin(GL_POINTS);
+        for (const auto& ray : rays) {
+            glVertex2f(ray.x, ray.y);
+        }
+        glEnd();
 
-			}
-			glEnd();
-		}
-		glDisable(GL_BLEND);
-	}
-	void step(double r_s, double rateOfChange) { //calculates the next position of the ray using the direction vector (for ray direction) and the speed of light
-		//r = hypot(x, y); //calculates distance from black hole to ray. This is also done here to update the distance after each step
-		if (r < r_s) return; //if the ray is within the event horizon, it will not be drawn anymore
-		rk4(*this, rateOfChange, r_s);
-		x = r * cos(phi);
-		y = r * sin(phi);
-		trails.push_back({ float(x), float(y) });
-		/*
-		x += direction.x * c;
-		y += direction.y * c;
-		
-		dr += d2r * rateOfChange; //update first derivative using second derivative
-		dphi += d2phi * rateOfChange;
-		r = dr * rateOfChange;
-		phi = dphi * rateOfChange;
-		
-		
-		x = cos(phi) * r;
-		y = sin(phi) * r; //convert polar coordinates back to cartesian coordinates
-		
-		cout << x << " " << y << endl;
-		trails.push_back(vec2(x, y)); //push the new position to the trails vector to get the path of the ray shown
-		*/
-	}
+        // turn on blending for the trails
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glLineWidth(1.0f);
+
+        // draw each trail with fading alpha
+        for (const auto& ray : rays) {
+            size_t N = ray.trail.size();
+            if (N < 2) continue;
+
+            glBegin(GL_LINE_STRIP);
+            for (size_t i = 0; i < N; ++i) {
+                // older points (i=0) get alpha≈0, newer get alpha≈1
+                float alpha = float(i) / float(N - 1);
+                glColor4f(1.0f, 1.0f, 1.0f, std::max(alpha, 0.05f));
+                glVertex2f(ray.trail[i].x, ray.trail[i].y);
+            }
+            glEnd();
+        }
+
+        glDisable(GL_BLEND);
+    }
+    void step(double dλ, double rs) {
+        // 1) integrate (r,φ,dr,dφ)
+        if (r <= rs) return; // stop if inside the event horizon
+        rk4Step(*this, dλ, rs);
+
+        // 2) convert back to cartesian x,y
+        x = r * cos(phi);
+        y = r * sin(phi);
+
+        // 3) record the trail
+        trail.push_back({ float(x), float(y) });
+    }
 };
-
-Engine engine;
-
 vector<Ray> rays;
-void geodesic(const Ray& ray, double rhs[4], double r_s) { //geodesic means the shortest distance in a curved space
-	double r = ray.r;
-	double phi = ray.phi;
-	double dr = ray.dr;
-	double dphi = ray.dphi;
-	double E = ray.E;
-	double f = 1.0 - r_s / r;
-	rhs[0] = dr;
-	rhs[1] = dphi;
-	double dt_dl = E / f;
-	rhs[2] = -(r_s / (2 * r * r)) * f * (dt_dl * dt_dl)
-		+ (r_s / (2 * r * r * f)) * (dr * dr) + (r - r_s) * (dphi * dphi);
-	rhs[3] = -2.0 * dr * dphi / r;
-	//ray.dr += r * dphi * dphi - (c * c * r_s) / (2.0 * r * r); //use of geodesic equation to calculate the acceleration of r (how close to moves to black hole)
-	//ray.dphi = -2.0 * dr * dphi / r; //use of geodesic equation to calculate the acceleration of phi, how fast the angle changes relative to black hole
 
-	//ray.d2r = r * dphi * dphi - (c * c * r_s) / (2.0 * r * r);
-	//ray.d2phi = -2.0 * dr * dphi / r;
-	//both these equations help calculate shortest possible distance in curved space time
+void geodesicRHS(const Ray& ray, double rhs[4], double rs) {
+    double r = ray.r;
+    double dr = ray.dr;
+    double dphi = ray.dphi;
+    double E = ray.E;
+
+    double f = 1.0 - rs / r;
+
+    // dr/dλ = dr
+    rhs[0] = dr;
+    // dφ/dλ = dphi
+    rhs[1] = dphi;
+
+    // d²r/dλ² from Schwarzschild null geodesic:
+    double dt_dλ = E / f;
+    rhs[2] =
+        -(rs / (2 * r * r)) * f * (dt_dλ * dt_dλ)
+        + (rs / (2 * r * r * f)) * (dr * dr)
+        + (r - rs) * (dphi * dphi);
+
+    // d²φ/dλ² = -2*(dr * dphi) / r
+    rhs[3] = -2.0 * dr * dphi / r;
 }
 void addState(const double a[4], const double b[4], double factor, double out[4]) {
-	for (int i = 0; i < 4; i++) {
-		out[i] = a[i] + b[i] * factor;
-	}
+    for (int i = 0; i < 4; i++)
+        out[i] = a[i] + b[i] * factor;
 }
-void rk4(Ray& ray, double timeStep, double r_s) {
-	double y0[4] = { ray.r, ray.phi, ray.dr, ray.dphi };
-	double k1[4];
-	double k2[4];
-	double k3[4];
-	double k4[4];
-	double temp[4];
-	geodesic(ray, k1, r_s);
-	addState(y0, k1, timeStep / 2.0, temp);
-	Ray r2 = ray; r2.r = temp[0]; r2.phi = temp[1]; r2.dr = temp[2]; r2.dphi = temp[3];
-	geodesic(r2, k2, r_s);
-	addState(y0, k2, timeStep / 2.0, temp);
-	Ray r3 = ray; r3.r = temp[0]; r3.phi = temp[1]; r3.dr = temp[2]; r3.dphi = temp[3];
-	geodesic(r3, k3, r_s);
-	addState(y0, k3, timeStep / 2.0, temp);
-	Ray r4 = ray; r4.r = temp[0]; r4.phi = temp[1]; r4.dr = temp[2]; r4.dphi = temp[3];
-	geodesic(r4, k4, r_s);
-	ray.r += (timeStep / 6.0) * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
-	ray.phi += (timeStep / 6.0) * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]);
-	ray.dr += (timeStep / 6.0) * (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]);
-	ray.dphi += (timeStep / 6.0) * (k1[3] + 2 * k2[3] + 2 * k3[3] + k4[3]);
+void rk4Step(Ray& ray, double dλ, double rs) {
+    double y0[4] = { ray.r, ray.phi, ray.dr, ray.dphi };
+    double k1[4], k2[4], k3[4], k4[4], temp[4];
+
+    geodesicRHS(ray, k1, rs);
+    addState(y0, k1, dλ / 2.0, temp);
+    Ray r2 = ray; r2.r = temp[0]; r2.phi = temp[1]; r2.dr = temp[2]; r2.dphi = temp[3];
+    geodesicRHS(r2, k2, rs);
+
+    addState(y0, k2, dλ / 2.0, temp);
+    Ray r3 = ray; r3.r = temp[0]; r3.phi = temp[1]; r3.dr = temp[2]; r3.dphi = temp[3];
+    geodesicRHS(r3, k3, rs);
+
+    addState(y0, k3, dλ, temp);
+    Ray r4 = ray; r4.r = temp[0]; r4.phi = temp[1]; r4.dr = temp[2]; r4.dphi = temp[3];
+    geodesicRHS(r4, k4, rs);
+
+    ray.r += (dλ / 6.0) * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
+    ray.phi += (dλ / 6.0) * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]);
+    ray.dr += (dλ / 6.0) * (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]);
+    ray.dphi += (dλ / 6.0) * (k1[3] + 2 * k2[3] + 2 * k3[3] + k4[3]);
 }
+Engine engine;
+
 int main() {
 	engine.setUp();
-	for (float y = -engine.h; y < engine.h; y += 1e10) {
-		rays.push_back(Ray(vec2(-engine.w, y), vec2(c, 0.0)));
-	}
+    for (float y = -engine.h; y < engine.h; y += 1e10) {
+        rays.push_back(Ray(vec2(-engine.w, y), vec2(c, 0.0)));
+    }
 	
 	while (!glfwWindowShouldClose(engine.window)) {
 		glClear(GL_COLOR_BUFFER_BIT); //frame must be cleared each frame
 		glLoadIdentity(); //identity matrix must be reloaded each frame
 		//engine.run();
-		hole.drawBlackHole();
+		hole.draw();
 		for (auto& ray: rays) {
 			ray.step(1.0f, hole.r_s);
-			ray.drawRay(rays);
+			ray.draw(rays);
 		}
 		glfwSwapBuffers(engine.window);
 		glfwPollEvents();
